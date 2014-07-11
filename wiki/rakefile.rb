26bd5c10a277a606bd24ca4340c7a29fcaa194d7
tsu-nera <fox10225fox@gmail.com>
2014-07-11 14:26:14 +0900

    rakefile改良

--- wiki/rakefile.rb
+++ wiki/rakefile.rb
@@ -10,6 +10,8 @@ BUILDDIR      = "pages"
 BUILDCMD      = "org-wk-export-to-wiki"
 URL           = "http://futurismo.biz/dokuwiki"
 
+task :default => [:export, :ftp]
+
 desc "export org-file to dokuwiki-file"
 task :export do
   cmd = "./#{BUILDCMD} #{SOURCEDIR}/*.org"
@@ -25,33 +27,3 @@ task :ftp do
     puts "FTP finished. Access to #{URL}"
   end
 end
-
-
-##################################################################
-# For Sphinx Setting(old)
-##################################################################
-# SPHINXOPTS    = ""
-# SPHINXBUILD   = "sphinx-build"
-# ALLSPHINXOPTS = "-d #{BUILDDIR}/doctrees #{SPHINXOPTS} #{SOURCEDIR}"
-# BROWSER       = "firefox"
-
-# desc "to make standalone HTML files"
-# task :html do
-#   cmd = "#{SPHINXBUILD} -b html #{ALLSPHINXOPTS} #{BUILDDIR}/html"
-#   if system( cmd )
-#     puts "Build finished. The HTML pages are in #{BUILDDIR}/html."
-#   end
-# end
-
-# desc "send all files to ftp server"
-# task :ftp do
-#   if system( "ncftpput -R lolipop ./wordpress/sphinx build/html" )
-#     puts "FTP finished. Access to http://futurismo.biz/sphinx/html/"
-#   end
-# end
-
-# desc "open local file"
-# task :open do
-#   cmd = "#{BROWSER} #{BUILDDIR}/html/index.html"
-#   system( cmd )
-# end
