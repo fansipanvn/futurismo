b0e566a4097afeed423e7ef276949ad0217b9445
tsu-nera <fox10225fox@gmail.com>
2014-09-13 17:55:33 +0900

    階層構造を導入

--- wiki/rakefile.rb
+++ wiki/rakefile.rb
@@ -1,31 +1,38 @@
 # Rakefile for Sphinx documentation
 require 'rake/clean'
+require 'syncftp'
 require 'pp'
 
 CLEAN.include(["*/**/*.~","*/**/\#*"])
-CLOBBER.include("*/**/*.txt")
+CLOBBER.include(["pages/**/*.~","pages/**/\#*","pages/**/*.org"])
 
 SOURCEDIR     = "src"
 BUILDDIR      = "pages"
+REMOTEDIR     = "./wordpress/dokuwiki/data"
 BUILDCMD      = "org-wk-export-to-wiki"
 
 BROWSER       = "firefox"
 URL           = "http://futurismo.biz/dokuwiki"
 
-task :default => [:export, :ftp]
+SRCS = FileList["**/*.org"]
+SRCS.exclude("README.org")
+WIKIS = SRCS.ext('txt')
 
-desc "export org-file to dokuwiki-file"
-task :export do
-  cmd = "./#{BUILDCMD} #{SOURCEDIR}/*.org"
-  if system( cmd )
-    system("mv #{SOURCEDIR}/*.txt #{BUILDDIR}")
-    puts "Build finished. The HTML pages are in #{BUILDDIR}"
-  end
+task :default => [:build, :ftp]
+
+directory BUILDDIR
+file :build => WIKIS do
+  system("rm -rf #{BUILDDIR}")
+  system("cp -r #{SOURCEDIR} #{BUILDDIR}")
+end
+
+rule '.txt' => '.org' do |t|
+  sh "./#{BUILDCMD} #{t.source}"
 end
 
 desc "send all files to ftp server"
-task :ftp do
-  if system( "ncftpput -R lolipop ./wordpress/dokuwiki/data pages" )
+task :ftp => :clobber do
+  if system( "ncftpput -R -m lolipop #{REMOTEDIR} #{BUILDDIR}" )
     puts "FTP finished. Access to #{URL}"
   end
 end
