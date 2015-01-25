f375845d62c5480b0801571d269a915da6417de4
tsu-nera <fox10225fox@gmail.com>
2015-01-25 09:49:09 +0900

    update

--- blog/archive/MyErrorSample.java
+++ blog/archive/MyErrorSample.java
@@ -1,5 +1,8 @@
 import java.lang.Exception;
 public class MyErrorSample {
+  static final int TEST = 1;
+  static final int TESTTEST = 1;
+  static final int TESTTESTTTTT = 1;
 	public static void main (String args[]) {
 		try {
 			throw new MyException (5);
